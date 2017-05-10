3b3924b18143b13645704f32e638aeba3c806cfb
Jamie Pitts <jamie.pitts@bbc.co.uk>
2017-05-10 21:00:38 +0100

    added context to webhook output

--- app.py
+++ app.py
@@ -31,9 +31,7 @@ def status():
 
 @app.route('/launches/next', methods=['GET'])
 def launches_next():
-    next_launch_string = getNextLaunchString()
-    print("Returning result: " + next_launch_string)
-    return next_launch_string
+    return to_json_response(getNextLaunchString())
 
 
 @app.route('/webhook', methods=['POST'])
@@ -45,7 +43,11 @@ def webhook():
 
     res = processRequest(req)
 
-    res = json.dumps(res, indent=4)
+    return to_json_response(res)
+
+
+def to_json_response(data):
+    res = json.dumps(data, indent=4)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
@@ -59,31 +61,32 @@ def getNextLaunchString():
     rocket_name = launch['rocket']['name']
     rocket_img_url = launch['rocket']['imageURL']
     mission_name = launch['missions'][0]['name']
+    launch_id = launch['id']
     launch_date = launch['net']
     launch_window_calc = launch['westamp'] - launch['wsstamp']
     launch_window = 'an instantaneous window' if launch_window_calc == 0 else 'a window of {} minutes'.format(launch_window_calc/60)
     launch_location = launch['location']['pads'][0]['name']
     formatted_string = 'The next SpaceX launch will be the {} rocket, performing the {} mission. The launch is planned for {}, with {}, flying from {}.'.format(rocket_name, mission_name, launch_date, launch_window, launch_location)
-    return formatted_string
+    return makeWebhookResult(formatted_string, [{"name": "launch", "lifespan": 5, "parameters": {"id": launch_id}}])
 
 
 
 def processRequest(req):
     action = req.get("result").get("action")
     if action == 'nextLaunch':
-        speech_string = getNextLaunchString()
+        return getNextLaunchString()
     else:
         return {}
 
-    return makeWebhookResult(speech_string)
 
-
-def makeWebhookResult(speech_string):
+def makeWebhookResult(speech_string, output_context):
     print("Response: " + speech_string)
 
     return {
         "speech": speech_string,
         "displayText": speech_string,
+        "contextOut": output_context,
+        "data": [],
         "source": "com.jamiepitts.space-chatbot",
         "messages": [
             {
